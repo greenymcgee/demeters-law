f1faac059ee297a15955f0a1ed7b127d7fab0c57
Houston Green <greenhouston@gmail.com>
2023-08-23 09:42:48 -0600

    #54 Adds Docker and Docker-Compose

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const BUILD_TIME = new Date().toLocaleString('en-US', {
+  timeZone: 'America/Chicago',
+})
+
 const nextConfig = {
   experimental: { typedRoutes: true },
 
-  env: {
-    BUILD_TIME: new Date().toLocaleString('en-US', {
-      timeZone: 'America/Chicago',
-    }),
-  },
+  env: { BUILD_TIME },
+
+  output: 'standalone',
 }
 
 module.exports = nextConfig
