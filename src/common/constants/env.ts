75abdbbcf1f2d1b6c1ac0fafd408762317e784b0
Houston Green <greenhouston@gmail.com>
2023-08-29 09:29:15 -0600

    #19 Adds Fix for NODE_ENV

--- src/common/constants/env.ts
+++ src/common/constants/env.ts
@@ -1,4 +1,5 @@
-export const { NODE_ENV } = process.env
+// eslint-disable-next-line prefer-destructuring
+export const NODE_ENV = process.env.NODE_ENV
 export const INTERNAL_API_HOST = process.env.NEXT_PUBLIC_INTERNAL_API_HOST
 export const COMMIT_SHA = process.env.NEXT_PUBLIC_COMMIT_SHA
 export const BUILD_TIME = process.env.NEXT_PUBLIC_BUILD_TIME
