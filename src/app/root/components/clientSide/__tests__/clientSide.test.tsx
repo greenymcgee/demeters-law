a694acb6bfd833dd373bce2067d1c9ff04af0d3e
Houston Green <greenhouston@gmail.com>
2023-08-29 11:00:45 -0600

    #19 Adds Fix for Test

--- src/app/root/components/clientSide/__tests__/clientSide.test.tsx
+++ src/app/root/components/clientSide/__tests__/clientSide.test.tsx
@@ -1,10 +1,4 @@
 import React from 'react'
-import {
-  BUILD_TIME,
-  COMMIT_SHA,
-  NODE_ENV,
-  VERCEL_ENV,
-} from '@/common/constants'
 import { logger } from '@/log'
 import { render } from '@testing-library/react'
 import { ClientSide } from '../..'
@@ -12,9 +6,6 @@ import { ClientSide } from '../..'
 describe('<ClientSide /> tests', () => {
   it('should run logs', () => {
     render(<ClientSide />)
-    expect(logger.info).toHaveBeenCalledWith(
-      { BUILD_TIME, COMMIT_SHA, NODE_ENV, VERCEL_ENV },
-      "Demeter's Law loaded",
-    )
+    expect(logger.info).toHaveBeenCalledWith("Demeter's Law loaded")
   })
 })
